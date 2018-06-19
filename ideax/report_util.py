07495a6094798e30997201b99660c735f0c361c1
Carlos Tosta <jctosta86@gmail.com>
2018-06-19 16:07:13 -0300

    This commit is focused on two main issues:
    - Audit Log (Issue #28)
    - Report Generation (Issue #20)
    - Log messages with the name `audit_log` are being written to the file `audit.log`
    - PDF generation method has been replaced with native javascript print function

--- ideax/report_util.py
+++ ideax/report_util.py
@@ -1,8 +1,8 @@
-from weasyprint import HTML, CSS
-from django.template.loader import render_to_string
+# from weasyprint import HTML, CSS
+# from django.template.loader import render_to_string
 
-def generate_pdf_report(template, absolute_uri, data=None):
-    html_string = render_to_string(template, data)
-    html = HTML(string=html_string, base_url=absolute_uri)
-    main_doc = html.render()
-    return main_doc.write_pdf();
+# def generate_pdf_report(template, absolute_uri, data=None):
+#     html_string = render_to_string(template, data)
+#     html = HTML(string=html_string, base_url=absolute_uri)
+#     main_doc = html.render()
+#     return main_doc.write_pdf();
