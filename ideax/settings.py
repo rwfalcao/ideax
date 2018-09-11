a0480f3f3354394b2b4f9f288d592d9a4049513e
Izabela Ramos <izabela.head@gmail.com>
2018-09-11 08:32:30 -0300

    changes in settings.py

--- ideax/settings.py
+++ ideax/settings.py
@@ -26,10 +26,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-ALLOWED_HOSTS = ['127.0.0.1']
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
