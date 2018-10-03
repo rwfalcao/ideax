e7bf5687dbd71d6b7aaf40675d3c2dd3109709f2
Juracy Filho <juracy@gmail.com>
2018-10-03 12:18:38 -0300

    New settings and packages for dev
    - django-extensions
    - ipython

--- ideax/settings/dev.py
+++ ideax/settings/dev.py
@@ -1,2 +1,8 @@
 from ._base import *
 
+INSTALLED_APPS += (
+    'django_extensions',
+)
+
+# Configurações de E-mail para dev
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
