75326857f86cb1a3da53a42e7a23ed05d57202b2
Juracy Filho <juracy@gmail.com>
2018-10-04 15:08:47 -0300

    Basic tests for ChallengeForm

--- ideax/pytest/fixtures.py
+++ ideax/pytest/fixtures.py
@@ -1,6 +1,14 @@
+from pathlib import Path
 from pytest import fixture
 
 
 @fixture
 def ideax():
     return 2
+
+
+@fixture
+def test_image(settings):
+    image = Path(settings.STATIC_ROOT) / 'images/favico.png'
+    with open(image, 'rb') as f:
+        return f.read()
