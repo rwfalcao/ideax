b5a9b51f659124cf41c7dd2952a1b5bce8ae1fdf
Juracy Filho <juracy@gmail.com>
2018-10-04 16:45:05 -0300

    Basic unit test for views

--- ideax/pytest/fixtures.py
+++ ideax/pytest/fixtures.py
@@ -12,3 +12,23 @@ def test_image(settings):
     image = Path(settings.STATIC_ROOT) / 'images/favico.png'
     with open(image, 'rb') as f:
         return f.read()
+
+
+class FakeMessages:
+    ''' mocks the Django message framework, makes it easier to get
+    the messages out '''
+
+    def __init__(self):
+        self.messages = []
+
+    def add(self, level, message, extra_tags):
+        self.messages.append(str(message))
+
+    @property
+    def pop(self):
+        return self.messages.pop()
+
+
+@fixture(scope='function')
+def messages():
+    return FakeMessages()
---
+++ ideax/tests/test_views.py
@@ -0,0 +1,30 @@
+from pytest import fixture
+
+from ideax.views import accept_use_term
+
+
+class TestViews:
+    @fixture
+    def get_ip(self, mocker):
+        patch = mocker.patch('ideax.views.get_ip')
+        patch.return_value = '1.1.1.1'
+        return patch
+
+    def test_accept_use_term_not_accepted(self, rf, admin_user, messages, get_ip):
+        admin_user.userprofile.use_term_accept = False
+        request = rf.get('/term/accept')
+        request.user = admin_user
+        request._messages = messages
+        response = accept_use_term(request)
+        assert response.status_code, response.url == (302, '/')
+        assert messages.messages == ['Termo de uso aceito!']
+        get_ip.assert_called_once_with(request)
+
+    def test_accept_use_term_accepted(self, rf, admin_user, messages):
+        admin_user.userprofile.use_term_accept = True
+        request = rf.get('/term/accept')
+        request.user = admin_user
+        request._messages = messages
+        response = accept_use_term(request)
+        assert response.status_code, response.url == (302, '/')
+        assert messages.messages == ['Termo de uso já aceito!']
