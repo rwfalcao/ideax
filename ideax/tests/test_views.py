93c81109a453597395360929224b9f4ec47f7ad8
Juracy Filho <juracy@gmail.com>
2018-10-05 12:30:25 -0300

    Minor view tests refactor

--- ideax/tests/test_views.py
+++ ideax/tests/test_views.py
@@ -1,7 +1,8 @@
-from model_mommy import mommy
 from django.contrib.auth.models import AnonymousUser
+from model_mommy import mommy
+from pytest import fixture
 
-from ideax.views import accept_use_term, index, idea_list
+from ideax.views import accept_use_term, idea_list, index
 
 
 class TestIndexView:
@@ -50,19 +51,25 @@ class TestAcceptUseTermView:
 
 
 class TestIdeaListView:
+    @fixture
+    def get_ideas_init(self, mocker):
+        return mocker.patch('ideax.views.get_ideas_init')
+
+    @fixture
+    def get_phases_count(self, mocker):
+        return mocker.patch('ideax.views.get_phases_count')
+
     def test_idea_list_anonymous(self, rf):
         request = rf.get('/idea/list')
         request.user = AnonymousUser()
         response = idea_list(request)
         assert (response.status_code, response.url) == (302, '/accounts/login/?next=/idea/list')
 
-    def test_idea_list_empty(self, rf, admin_user, mocker):
-        get_ideas_init = mocker.patch('ideax.views.get_ideas_init')
+    def test_idea_list_empty(self, get_ideas_init, get_phases_count, rf, admin_user, mocker):
         get_ideas_init.return_value = {
             'ideas': [],
             'challenges': [],
         }
-        get_phases_count = mocker.patch('ideax.views.get_phases_count')
         get_phases_count.return_value = 5
         request = rf.get('/idea/list')
         request.user = admin_user
@@ -70,13 +77,11 @@ class TestIdeaListView:
         assert response.status_code == 200
         assert 'Não existem ideias nesta etapa!' in response.content.decode('utf-8', 'strict')
 
-    def test_idea_list(self, rf, admin_user, mocker):
-        get_ideas_init = mocker.patch('ideax.views.get_ideas_init')
+    def test_idea_list(self, get_ideas_init, get_phases_count, rf, admin_user, mocker):
         get_ideas_init.return_value = {
             'ideas': [mommy.make('Idea')],
             'challenges': [],
         }
-        get_phases_count = mocker.patch('ideax.views.get_phases_count')
         get_phases_count.return_value = 5
         request = rf.get('/idea/list')
         request.user = admin_user
