54c99bede12ce5b4c1299390552b7252d7f68e55
Juracy Filho <juracy@gmail.com>
2018-10-18 08:31:36 -0300

    Tests for all possible return of save_use_term

--- ideax/ideax/tests/views/test_accept_use_term.py
+++ ideax/ideax/tests/views/test_accept_use_term.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import PermissionDenied
 from django.http.response import Http404
+from django.utils import timezone
 
 from model_mommy import mommy
-from pytest import raises
+from pytest import mark, raises
 
 from ...forms import UseTermForm
 from ...views import accept_use_term, save_use_term, use_term_edit
@@ -99,3 +102,63 @@ class TestSaveUseTerm:
         request.user = admin_user
         save_use_term(request, 'form', 'use_term_edit.html')
         render.assert_called_once_with(request, 'use_term_edit.html', {'form': 'form'})
+
+    def test_post_1(self, rf, admin_user, mocker, messages):
+        form = mocker.patch('ideax.ideax.forms.UseTermForm')
+        form.is_valid.return_value = True
+        form.save.return_value.is_invalid_date.return_value = True
+        terms = mocker.patch('ideax.ideax.models.Use_Term.objects')
+        terms.all.return_value = []
+        render = mocker.patch('ideax.ideax.views.render')
+        request = rf.post('/', {})
+        request.user = admin_user
+        request._messages = messages
+        save_use_term(request, form, 'use_term_edit.html')
+        render.assert_called_once_with(request, 'use_term_edit.html', {'form': form})
+        assert messages.messages == ['Invalid Final Date']
+
+    def test_post_new(self, rf, admin_user, mocker, messages):
+        form = mocker.patch('ideax.ideax.forms.UseTermForm')
+        form.is_valid.return_value = True
+        form.save.return_value.is_invalid_date.return_value = False
+        terms = mocker.patch('ideax.ideax.models.Use_Term.objects')
+        # use_term = mocker.Mock()
+        # use_term.is_past_due.return_value = True
+        use_term = mommy.make('Use_Term', final_date=timezone.make_aware(datetime(2099, 12, 31)))
+        terms.all.return_value = [use_term]
+        render = mocker.patch('ideax.ideax.views.render')
+        request = rf.post('/', {})
+        request.user = admin_user
+        request._messages = messages
+        save_use_term(request, form, 'use_term_edit.html', True)
+        render.assert_called_once_with(request, 'use_term_edit.html', {'form': form})
+        assert messages.messages == ['Already exists a active Term Of Use']
+
+    def test_post_new_inactive(self, rf, admin_user, mocker, messages):
+        form = mocker.patch('ideax.ideax.forms.UseTermForm')
+        form.is_valid.return_value = True
+        form.save.return_value.is_invalid_date.return_value = False
+        terms = mocker.patch('ideax.ideax.models.Use_Term.objects')
+        # use_term = mocker.Mock()
+        # use_term.is_past_due.return_value = False
+        use_term = mommy.make('Use_Term', final_date=timezone.make_aware(datetime(2009, 12, 31)))
+        terms.all.return_value = [use_term]
+        # render = mocker.patch('ideax.ideax.views.render')
+        request = rf.post('/', {})
+        request.user = admin_user
+        request._messages = messages
+        response = save_use_term(request, form, 'use_term_edit.html', True)
+        assert (response.status_code, response.url) == (302, '/useterm/list/')
+        # render.assert_called_once_with(request, 'use_term_edit.html', {'form': form})
+        assert messages.messages == ['Term of Use saved successfully!']
+
+    @mark.skip('TODO: save use term with invalid form')
+    def test_post_form_invalid(self, rf, admin_user, mocker):
+        form = mocker.patch('ideax.ideax.forms.UseTermForm')
+        form.is_valid.return_value = False
+        form.save.return_value.is_invalid_date.return_value = False
+        request = rf.post('/', {})
+        request.user = admin_user
+        response = save_use_term(request, form, 'use_term_edit.html', True)
+        # TODO: maybe a message
+        assert response is not None
