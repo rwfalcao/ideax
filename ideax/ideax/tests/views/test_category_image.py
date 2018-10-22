0a3b66610764d73a7dca5fa58d951273e51ac274
Izabela Ramos <izabela.head@gmail.com>
2018-10-22 10:35:10 -0300

    Tests for Category Image #94

---
+++ ideax/ideax/tests/views/test_category_image.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import PermissionDenied
+from django.http.response import Http404
+
+from model_mommy import mommy
+from pytest import raises
+
+from ...forms import CategoryForm
+from ...views import (category_image_edit, category_image_remove)
+
+class TestCategoryImageEdit:
+    def test_anonymous(self, rf):
+        request = rf.get(f'/categoryimage/99999/edit/')
+        request.user = AnonymousUser()
+        response = category_image_edit(request, 99999)
+        assert (response.status_code, response.url) == (302, '/accounts/login/?next=/categoryimage/99999/edit/')
+
+    def test_not_found(self, rf, admin_user):
+        request = rf.get(f'/categoryimage/99999/edit/')
+        request.user = admin_user
+        with raises(Http404):
+            category_image_edit(request, 99999)
+
+    def test_get_common_user(self, rf, common_user):
+        request = rf.get('/categoryimage/1/edit/')
+        request.user = common_user
+        with raises(PermissionDenied):
+            category_image_edit(request, 1)
+
+class TestCategoryImageRemove:
+    def test_anonymous(self, rf):
+        request = rf.get('/')
+        request.user = AnonymousUser()
+        response = category_image_remove(request, 999)
+        assert (response.status_code, response.url) == (302, '/accounts/login/?next=/')
+    
+    def test_common_user(self, rf, common_user):
+        request = rf.get('/')
+        request.user = common_user
+        with raises(PermissionDenied):
+            category_image_remove(request, 1)
