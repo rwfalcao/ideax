acfca9df149372d54b6ff95cf46522f4ff5ac9d7
Izabela Ramos <izabela.head@gmail.com>
2018-10-23 10:17:26 -0300

    solving conflicts

--- ideax/ideax/tests/models/test_category.py
+++ ideax/ideax/tests/models/test_category.py
@@ -1,10 +1,5 @@
 from model_mommy import mommy
 from pytest import fixture
-<<<<<<< HEAD
-from model_mommy import mommy
-=======
-
->>>>>>> de304ef8db3345d89b7a9a88f5e88cdd63a2613b
 from ...models import Category
 
 
@@ -23,16 +18,6 @@ class TestCategory:
     def test_get_images_empty(self, category):
         assert not category.get_all_image_header()
 
-<<<<<<< HEAD
-    def test_get_images_empty(self, setup_category):
-        assert not setup_category.get_all_image_header()
-
-    def test_get_images(self, setup_category):
-        image = mommy.make('Category_Image', category=setup_category)
-        assert setup_category.get_all_image_header()
-        assert setup_category.get_all_image_header()[0].description == image.description
-=======
     def test_get_images(self, category):
         mommy.make('Category_Image', category=category)
         assert category.get_all_image_header()
->>>>>>> de304ef8db3345d89b7a9a88f5e88cdd63a2613b
