52e0c39fde7a25c0203e04b282d5ad74424c3f3a
Juracy Filho <juracy@gmail.com>
2018-10-26 09:10:15 -0300

    Test users app urls

---
+++ ideax/users/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.urls import reverse
+
+
+class TestUsersUrls:
+    namespace = 'users'
+
+    def test_login(self):
+        assert reverse(f'{self.namespace}:login') == '/accounts/login/'
+
+    def test_logout(self):
+        assert reverse(f'{self.namespace}:logout') == '/accounts/logout/'
+
+    def test_profile(self):
+        assert reverse(f'{self.namespace}:profile') == '/users/profile/'
