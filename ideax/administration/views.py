0fcdd7110543c0196c3cfd81a09461f9c9d2c44b
Izabela Ramos <izabela.head@gmail.com>
2018-11-05 14:05:13 -0200

    Linting

--- ideax/administration/views.py
+++ ideax/administration/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def administration(request):
-    #audit(request.user.username, get_client_ip(request), 'ADMINISTRATION_PAGE', Idea.__name__, '')
+    # audit(request.user.username, get_client_ip(request), 'ADMINISTRATION_PAGE', Idea.__name__, '')
     return render(request, 'administration/index.html')
