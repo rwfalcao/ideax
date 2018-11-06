914bb134b538ed8f512cb07521dbed5327c74969
Izabela Ramos <izabela.head@gmail.com>
2018-11-06 12:40:03 -0200

    FIXME in administration/views

--- ideax/administration/views.py
+++ ideax/administration/views.py
@@ -4,5 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def administration(request):
+    # FIXME after easy access to audit function
     # audit(request.user.username, get_client_ip(request), 'ADMINISTRATION_PAGE', Idea.__name__, '')
     return render(request, 'administration/index.html')
