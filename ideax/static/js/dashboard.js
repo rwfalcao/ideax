8ee20571e128362b7fbe0ab1769ab1d3c0358d17
samyr <samyr.info@gmail.com>
2018-11-27 09:53:40 -0200

    Improving front-end page fixes#46

--- ideax/static/js/dashboard.js
+++ ideax/static/js/dashboard.js
@@ -2,37 +2,29 @@
 
 $('a#dashboardSort').click(function () {
 
-     var $divs = $(".profile-info");
+     var $divs = $(".innovator-info");
      if(this.name == 'thumbsUp'){
          var mostLikesOrderedDivs = $divs.sort(function (a, b) {
              return $(a).data('like') < $(b).data('like');
          });
          $("#innovator-container").html(mostLikesOrderedDivs);
-         $(".profile-info").wrapAll($("<div class='row mt-4'></div>"));
+         $(".innovator-info").wrapAll($("<div class='row mt-4'></div>"));
     }
      else if(this.name == 'thumbsDown'){
          var mostDislikesOrderedDivs = $divs.sort(function (a, b) {
-             return $(a).data('dislike') > $(b).data('dislike');
+             return $(a).data('dislike') < $(b).data('dislike');
          });
          $("#innovator-container").html(mostDislikesOrderedDivs);
-         $(".profile-info").wrapAll($("<div class='row mt-4'></div>"));
+         $(".innovator-info").wrapAll($("<div class='row mt-4'></div>"));
     }
 
-      else if(this.name == 'mostIdeas'){
+      else if(this.name == 'collaborators'){
           var maisIdeasOrderedDivs = $divs.sort(function (a, b) {
               return $(a).find('.qtdIdeas').text() < $(b).find('.qtdIdeas').text()
           });
           $("#innovator-container").html(maisIdeasOrderedDivs);
-          $(".profile-info").wrapAll($("<div class='row mt-4'></div>"));
+          $(".innovator-info").wrapAll($("<div class='row mt-4'></div>"));
      }
-
-     else if(this.name == 'fewerIdeas'){
-         var menosIdeasOrderedDivs = $divs.sort(function (a, b) {
-             return $(a).find('.qtdIdeas').text() > $(b).find('.qtdIdeas').text()
-         });
-         $("#innovator-container").html(menosIdeasOrderedDivs);
-         $(".profile-info").wrapAll($("<div class='row mt-4'></div>"));
-    }
 
      $('#dropdownMenuButton span.option').text($(this).text());
 });
