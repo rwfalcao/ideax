f16bf3cac193820ec1fc16d6c85eafe8291f632d
Izabela Ramos <izabela.head@gmail.com>
2019-01-18 13:42:31 -0200

    snapshots

--- ideax/ideax/tests/forms/snapshots/snap_test_category.py
+++ ideax/ideax/tests/forms/snapshots/snap_test_category.py
@@ -7,29 +7,11 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestCategoryForm.test_invalid 1'] = {
-    'description': [
-        'Este campo é obrigatório.'
-    ],
-    'title': [
-        'Este campo é obrigatório.'
-    ]
-}
-
-snapshots['TestCategoryForm.test_max 1'] = {
-    'description': [
-        'Certifique-se de que o valor tenha no máximo 200 caracteres (ele possui 201).'
-    ],
-    'title': [
-        'Certifique-se de que o valor tenha no máximo 50 caracteres (ele possui 51).'
-    ]
-}
-
 snapshots['TestCategoryForm.test_max_ptbr 1'] = {
     'description': [
-        'Certifique-se de que o valor tenha no máximo 200 caracteres (ele possui 201).'
+        'Ensure this value has at most 200 characters (it has 201).'
     ],
     'title': [
-        'Certifique-se de que o valor tenha no máximo 50 caracteres (ele possui 51).'
+        'Ensure this value has at most 50 characters (it has 51).'
     ]
 }
--- ideax/ideax/tests/forms/snapshots/snap_test_dimension.py
+++ ideax/ideax/tests/forms/snapshots/snap_test_dimension.py
@@ -9,33 +9,9 @@ snapshots = Snapshot()
 
 snapshots['TestDimensionForm.test_max_ptbr 1'] = {
     'description': [
-        'Certifique-se de que o valor tenha no máximo 500 caracteres (ele possui 501).'
+        'Ensure this value has at most 500 characters (it has 501).'
     ],
     'title': [
-        'Certifique-se de que o valor tenha no máximo 200 caracteres (ele possui 201).'
-    ]
-}
-
-snapshots['TestDimensionForm.test_invalid 1'] = {
-    'description': [
-        'Este campo é obrigatório.'
-    ],
-    'init_date': [
-        'Este campo é obrigatório.'
-    ],
-    'title': [
-        'Este campo é obrigatório.'
-    ],
-    'weight': [
-        'Este campo é obrigatório.'
-    ]
-}
-
-snapshots['TestDimensionForm.test_max 1'] = {
-    'description': [
-        'Certifique-se de que o valor tenha no máximo 500 caracteres (ele possui 501).'
-    ],
-    'title': [
-        'Certifique-se de que o valor tenha no máximo 200 caracteres (ele possui 201).'
+        'Ensure this value has at most 200 characters (it has 201).'
     ]
 }
