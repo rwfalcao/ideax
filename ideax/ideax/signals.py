89e6044a5197d63a49872b39a39f465fd6e70531
Izabela Ramos <izabela.head@gmail.com>
2019-01-29 10:27:22 -0200

    fix signals

--- ideax/ideax/signals.py
+++ ideax/ideax/signals.py
@@ -1,10 +1,20 @@
 from django.db.models.signals import post_save
 from notifications.signals import notify
-from .models import Popular_Vote
+from .models import Popular_Vote, Idea, Evaluation_Item, Phase, Comment
+from django.dispatch import receiver
+from django.core.signals import request_finished
 
 
 def my_handler(sender, instance, created, **kwargs):
-    notify.send(instance, verb='liked')
+    notify.send(instance, verb='like')
+    notify.send(instance, verb='evaluate')
+    notify.send(instance, verb='phase')
+    notify.send(instance, verb='comment')
+    notify.send(instance, verb='author')
 
 
 post_save.connect(my_handler, sender=Popular_Vote)
+post_save.connect(my_handler, sender=Idea)
+post_save.connect(my_handler, sender=Evaluation_Item)
+post_save.connect(my_handler, sender=Phase)
+post_save.connect(my_handler, sender=Comment)
